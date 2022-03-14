d20ae14c2d6af6f760dccfa24029e837836d374f
anniejoel <annhayriyan@gmail.com>
2022-03-15 00:33:38 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import Header from "./components/menu/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
+// import {getData} from "./services/api"
 
+// getData();
 function App() {
+ 
   return (
     <div className="App">
-      <Header />
-
-        <Footer />
+     <Header />
+     <Home />
+     <Footer />
     </div>
   );
 }
