e4b454bad54316d22f2ceeaf35b412d22de5a19d
anniejoel <annhayriyan@gmail.com>
2022-03-15 00:35:25 +0400

    first

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import Cards from "../card/Cards";
+import "../home/home.css";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+           <Cards />
+        </div>
+    );
+}
+
+export default Home;
